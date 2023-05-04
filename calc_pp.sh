6af7f28114046f2a542c9ee85dfc9e2653fac7dc
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-05-04 10:34:15 -0400

    Added calc_sp.sh and changed the name of ppp.sh to calc_pp.sh

--- calc_pp.sh
+++ calc_pp.sh
@@ -2,14 +2,14 @@
 
 # Author: Jose Michael Rubio
 # Date Created: 5/3/2023
-# Last Modified: 5/3/2023
+# Last Modified: 5/4/2023
 
 # Description:
 # Calculator that does basic arithemtic calculations (+,-). First arguement is the operatotion followed by a max of 9 numbers.
 ## All numbers will have the same operation performed upon it.
 
 # Usage:
-# ppp.sh
+# calc_pp.sh
 
 echo $((${2:-0} $1 ${3:-0} $1 ${4:-0} $1 ${5:-0} $1 ${6:-0} $1 ${7:-0} $1 ${8:-0} $1 ${9:-0} $1 ${10:-0} ))
 exit 0
---
+++ calc_sp.sh
@@ -0,0 +1,14 @@
+#!/opt/homebrew/bin/bash
+
+# Author: Jose Michael Rubio
+# Date Created: 5/3/2023
+# Last Modified: 5/4/2023
+
+# Description:
+# Using Special parameters,Calculator that does basic arithemtic calculations (+,-). First arguement is the operatotion followed by a max of 9 numbers.
+## All numbers will have the same operation performed upon it.
+
+# Usage:
+# calc_sp.sh
+
+echo $(($*))
