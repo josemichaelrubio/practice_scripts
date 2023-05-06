21b81b7196be62ca133ea1cddfe1e09e95dee5d7
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-05-06 17:25:13 -0400

    Added array folder and for_loop.sh

---
+++ for_loop.sh
@@ -0,0 +1,18 @@
+#!/opt/homebrew/bin/bash
+
+# Author: Jose Michael Rubio
+# Date Created: 5/6/2023
+# Last Modified: 5/6/2023
+
+# Description:
+# Studying iterating arrays with for loops
+
+# Usage:
+# for_loop.sh
+
+for element in first second third; do
+    echo "This is $element"
+done
+
+
+
