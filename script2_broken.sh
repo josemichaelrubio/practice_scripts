d5e547e73db4ad588848402544bae129d79ab704
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-05-06 22:13:49 -0400

    fixed script2_broken.sh

--- script2_broken.sh
+++ script2_broken.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
-if ! grep -q backup=true.* "~/.myconfig"
-then
-  echo 'Backup not enabled in $HOME/.myconfig, exiting'
+# Author: Jose Michael Rubio
+# Date Created: 5/6/2023
+# Last Modified: 5/6/2023
+
+# Description:
+# Use spellcheck to resolve errors on this broken script
+
+# Usage:
+# script2_broken.sh
+
+if ! grep -q backup="true.*"  "$HOME/.myconfig" ; then
+  echo "Backup not enabled in $HOME/.myconfig, exiting"
   exit 1
 fi
 
-tar -cf  “$1/my_backup_$(date +%d-%m-%Y_%H-%M-%S).tar” "$HOME"
+tar -cf  "$1/my_backup_$(date +%d-%m-%Y_%H-%M-%S).tar" "$HOME"
