943cf6e39a6db7572fa6bbae4c4ddc5273c70a14
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-05-07 02:17:12 -0400

    created folder_organizer.sh

---
+++ folder_organizer.sh
@@ -0,0 +1,18 @@
+#!/opt/homebrew/bin/bash
+
+# Author: Jose Michael Rubio
+# Date Created: 5/7/2023
+# Last Modified: 5/7/2023
+
+# Description:
+# 2nd script building assignment project. Oragnize files within a folder into subfolders based on file type
+
+# Usage:
+# folder_organizer.sh
+
+
+# Output of ls command to generate a list of files in the folder you are going to organize
+# While loop that iterates over the output of ls
+
+
+
