145f01cf58abcec3966dce9551f2d04b26f3f73f
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-05-07 02:12:31 -0400

    Completed cruft_remover.sh

--- cruft_remover.sh
+++ cruft_remover.sh
@@ -10,6 +10,32 @@
 # Usage:
 # cruft_remover.sh
 
+#ask the user which folder they want to remove cruft from
+#ask user to define how many days files should be unmodified to be consider cruft
+echo "Hello! Welcome to cruft remover!"
+echo "Cruft are unnecessary files that have not been modified in some time"
 
+read -p "Which folder do you want to remove cruft from? (path please!) " folder
+read -p "How many days would you consider an unmodified file cruft? " days
+
+# create an array of files eligble for removal
+
+find $folder -mtime -$days
+readarray -t files < <(find $folder -mtime -$days)
+
+# iterate over the array and present the user with an option to delete each file in the array
+for file in "${files[@]}"; do
+    PS3="Would you like to keep or delete $file?"
+    select opt in keep delete;
+        do
+            if [ $opt = "keep" ]; then
+                echo "$file kept."
+            else
+                rm $file
+                echo "$file was deleted."
+            fi
+        break
+    done
+done 
 
 
