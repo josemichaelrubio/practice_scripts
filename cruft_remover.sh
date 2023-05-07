106d877c1447cb178984a147e26b9017ed9dc3f1
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-05-07 01:33:25 -0400

    created cruft_remover.sh

---
+++ cruft_remover.sh
@@ -0,0 +1,15 @@
+#!/opt/homebrew/bin/bash
+
+# Author: Jose Michael Rubio
+# Date Created: 5/7/2023
+# Last Modified: 5/7/2023
+
+# Description:
+# First script building assignment. Deletes cruft, files that haven't been modified for a certain period of time
+
+# Usage:
+# cruft_remover.sh
+
+
+
+
