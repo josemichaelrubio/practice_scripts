b60e371671d8ae2291c2e7ffc909d4529c452e30
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-05-07 05:43:34 -0400

    Cleared folder_organizer.sh

--- folder_organizer.sh
+++ folder_organizer.sh
@@ -11,30 +11,8 @@
 # folder_organizer.sh
 
 
-# Output of ls command to generate a list of files in the folder you are going to organize
-readarray -t files < <(ls "$PWD")
-
 # While loop that iterates over the output of ls
-for file in "${files[@]}"; do
-    name=$(basename "$file")
-    ext="${name##*.}"
-
-    creation=$(if [ -e "$PWD/$OPTARG" ]; then
-        echo "$OPTARG directory exists"
-        mv "$file" "$PWD/$OPTARG"
-    else  
-        mkdir "$PWD/$OPTARG"
-         mv "$file" "$PWD/$OPTARG"
-    fi)
-
 # use case statement to globe the file names for their extentions and action commands based on their file extention
-    case $ext in 
-       .txt) $creation documents ;;
-
-    esac
-done 
-
-
 # logic to move each file into its appropriate folder, create a folder if it doesn't exist
 
 
