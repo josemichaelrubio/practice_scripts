5a73fe379e38898b775b0f31c428deef5507161b
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-05-24 05:22:51 -0400

    created at_script.sh

---
+++ at_script.sh
@@ -0,0 +1,14 @@
+#!/opt/homebrew/bin/bash
+
+# Author: Jose Michael Rubio
+# Date Created: 5/24/2023
+# Last Modified: 5/24/2023
+
+# Description:
+# Learning at command. How to schedule a script to happen at a given time.
+
+# Usage:
+# at_script.sh
+
+mkdir at_dir
+touch at_dir/file{1..100}.txt
